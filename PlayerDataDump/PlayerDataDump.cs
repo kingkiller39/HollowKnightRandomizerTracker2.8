9629cc508e31f2648abf960f2b29ddf3cfb49203
Jonathon Wyza <iamwyza@gmail.com>
2017-10-17 00:06:37 -0500

    Code for PlayerDataDump from @KayDeeTee

---
+++ PlayerDataDump/PlayerDataDump.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Modding;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using HutongGames.PlayMaker;
+using System.Reflection;
+using WebSocketSharp.Server;
+
+namespace PlayerDataDump
+{
+    public class PlayerDataDump : Mod
+    {
+        public WebSocketServer wss = new WebSocketServer(11420);
+
+        public override string GetVersion()
+        {
+            return "16/10/17.b";
+        }
+
+        public override void Initialize()
+        {
+            ModHooks.ModLog("Initializing PlayerDataDump");
+
+            //Setup websockets server
+             wss.AddWebSocketService<SocketServer>(
+              "/playerData",
+              () =>{
+                  SocketServer ss = new SocketServer()
+                  {
+                    IgnoreExtensions = true
+                  };
+
+                  ModHooks.Instance.NewGameHook += ss.newGame;
+                  ModHooks.Instance.SavegameLoadHook += ss.loadSave;
+                  ModHooks.Instance.SetPlayerBoolHook += ss.updateJson;
+                  ModHooks.Instance.SetPlayerIntHook += ss.updateJson;
+
+                  ModHooks.Instance.ApplicationQuitHook += ss.onQuit;
+
+                  return ss;
+              }
+            );
+            wss.Start();
+
+            ModHooks.ModLog("Initialized PlayerDataDump");
+        }
+    }
+}
---
+++ PlayerDataDump/SocketServer.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using Modding;
+using UnityEngine;
+using System.IO;
+
+namespace PlayerDataDump
+{
+    class SocketServer : WebSocketBehavior
+    {
+        public void Broadcast(String s)
+        {
+            Sessions.Broadcast(s);
+        }
+
+        
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            if (e.Data == "random")
+            {
+                Send(getRandom());
+            }
+            else if (e.Data == "json")
+            {
+                Send(getJson());
+            }
+            else
+            {
+                if (e.Data.Contains('|'))
+                {
+                    if (e.Data.Split('|')[0] == "bool")
+                    {
+                        string b = ModHooks.Instance.GetPlayerBool(e.Data.Split('|')[1]).ToString();
+                        Send("{"
+                        + "\"var\":\"" + e.Data.Split('|')[1] + "\","
+                        + "\"value\":\"" + b + "\""
+                        + "}"
+                        ); 
+                    }
+                    if (e.Data.Split('|')[0] == "int")
+                    {
+                        string i = ModHooks.Instance.GetPlayerInt(e.Data.Split('|')[1]).ToString();
+                        Send("{"
+                        + "\"var\":\"" + e.Data.Split('|')[1] + "\","
+                        + "\"value\":\"" + i + "\""
+                        + "}"
+                        ); 
+                    }
+                }
+                //Send("the only supported request is \"json\"");
+            }
+            ModHooks.ModLog("[PlayerDataDump] MSG: " + e.Data);
+            //Send("I'll be the one sending messages here");
+        }
+
+        protected override void OnError(WebSocketSharp.ErrorEventArgs e)
+        {
+            ModHooks.ModLog("[PlayerDataDump] ERROR: " + e.Message);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+
+            ModHooks.Instance.NewGameHook -= this.newGame;
+            ModHooks.Instance.SavegameLoadHook -= this.loadSave;
+            ModHooks.Instance.SetPlayerBoolHook -= this.updateJson;
+            ModHooks.Instance.SetPlayerIntHook -= this.updateJson;
+
+            ModHooks.Instance.ApplicationQuitHook -= this.onQuit;
+
+            ModHooks.ModLog("[PlayerDataDump] CLOSE: Code:" + e.Code + ", Reason:" + e.Reason);
+        }
+
+        protected override void OnOpen()
+        {
+            ModHooks.ModLog("[PlayerDataDump] OPEN");
+        }
+
+
+        public void loadSave(int slot)
+        {
+            Send("{"
+                + "\"var\":\"SaveLoaded\","
+                + "\"value\":\"true\""
+                + "}"
+                );
+        }
+
+        public void updateJson(string var, bool value)
+        {
+            if (var.StartsWith("gotCharm_") || var.StartsWith("equippedCharm_") || var.StartsWith("has") || var.StartsWith("maskBroken") || var == "overcharmed")
+            {
+                Send("{"
+                + "\"var\":\"" + var + "\","
+                + "\"value\":\"" + value.ToString() + "\""
+                + "}"
+                );
+            }
+        }
+
+        public void updateJson(string var, int value)
+        {
+            //trinket1, trinket2, trinket3, trinket4, ore, rancidEggs, & grubsCollected?
+            if (var.EndsWith("Level") || var == "simpleKeys" || var == "nailDamage" || var == "maxHealth" || var == "MPReserveMax" || var.StartsWith("trinket") || var == "ore" || var == "rancidEggs" || var == "grubsCollected")
+            {
+                Send("{"
+                + "\"var\":\"" + var + "\","
+                + "\"value\":\"" + value.ToString() + "\""
+                + "}"
+                );
+            }
+        }
+
+        public static string getJson()
+        {
+            PlayerData playerData = PlayerData.instance;
+            String json = JsonUtility.ToJson(playerData);
+
+            int randomFireballLevel = ModHooks.Instance.GetPlayerInt("_fireballLevel");
+            int randomQuakeLevel = ModHooks.Instance.GetPlayerInt("_quakeLevel");
+            int randomScreamLevel = ModHooks.Instance.GetPlayerInt("_screamLevel");
+
+            if (randomFireballLevel >= 0)
+            {
+                PlayerData pd = JsonUtility.FromJson<PlayerData>(json);
+                pd.fireballLevel = randomFireballLevel;
+                pd.quakeLevel = randomQuakeLevel;
+                pd.screamLevel = randomScreamLevel;
+                json = JsonUtility.ToJson(pd);
+            }
+
+            return json;
+        }
+
+        public static string getRandom()
+        {
+            String path = Application.persistentDataPath + "/rnd.js";
+            String data = File.ReadAllText(path);
+            return data;
+        }
+
+        public void newGame()
+        {
+            Send("{"
+    + "\"var\":\"NewSave\","
+    + "\"value\":\"true\""
+    + "}"
+    );
+        }
+
+
+        public void onQuit()
+        {
+            Send("{"
+                + "\"var\":\"GameExiting\","
+                + "\"value\":\"true\""
+                + "}"
+                );
+        }
+    }
+}
